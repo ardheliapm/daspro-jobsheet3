eeb63ee5c8033185a6489a8a1ee85e4ff7121fee
ardheliapm <96240717+ardheliapm@users.noreply.github.com>
2023-10-04 00:05:03 +0700

    Pengguna Variabel

---
+++ ContohVariabel05.java
@@ -0,0 +1,21 @@
+public class ContohVariabel05 {
+    public static void main(String[] args) {
+
+    String salahSatuHobiSayaAdalah = "Bermain petak umpet";
+    boolean isPandai = true;
+    char jenisKelamin = 'P';
+    byte _umurSayaSekarang = 18;
+    double $ipk = 4.00, tinggi = 1.67;
+
+    System.out.println(salahSatuHobiSayaAdalah);
+    System.out.println("Apakah pandai?" + isPandai);
+    System.out.println("Jenis kelamin: " + jenisKelamin);
+    System.out.println("IPK: " + $ipk);
+    System.out.println(String.format("saya berumur %s dengan tinggi badan %s", _umurSayaSekarang, tinggi));
+    }
+}
+
+  
+    
+    
+
