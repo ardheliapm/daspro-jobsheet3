7bed2fd6e8cc84f3568bc9939a7512aa3016b8e3
ardheliapm <96240717+ardheliapm@users.noreply.github.com>
2023-10-03 23:45:10 +0700

    Studi Kasus Luas Segitiga

---
+++ Segitiga05.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Segitiga05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int alas, tinggi;
+        float luas;
+
+        System.out.println("Masukan alas: ");
+        alas = scanner.nextInt();
+        System.out.println("Masukan tinggi: ");
+        tinggi = scanner.nextInt();
+
+        luas = alas * tinggi / 2;
+
+        System.out.println("Luas Segitiga" + luas);
+    }
+}
+
+
+
+
+ 
+    
