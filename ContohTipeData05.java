91cde15f43efbc4f3cb596866b9ce62879081ee9
ardheliapm <96240717+ardheliapm@users.noreply.github.com>
2023-10-03 23:50:17 +0700

    Delete ContohTipeData05.java

--- ContohTipeData05.java
+++
@@ -1,9 +0,0 @@
-{public static void main(String[] args)
-    char golonganDarah = 'A';
-    short jumlahPenduduk = 1025;
-    float suhu = 60,50f;
-    double berat = 0,5467812345;
-    long saldo = 150000000;
-    int angkaDesimal = 0x10;
-
-    System.out.println("Golongan darah\t: " (byte)golonganDarah);
